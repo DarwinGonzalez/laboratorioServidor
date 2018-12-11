09f15a7d63f62465c0ffd0178c16cd944b5d0874
Darwin Gonzalez <alu0100812794@ull.edu.es>
2018-12-11 18:16:22 +0100

    Add contador

--- ProcesamientoWorker/src/main/java/Consumidor.java
+++ ProcesamientoWorker/src/main/java/Consumidor.java
@@ -1,4 +1,3 @@
-
 import com.google.gson.Gson;
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
@@ -25,117 +24,128 @@ import org.opencv.core.Mat;
  */
 public class Consumidor {
 
-    public static void main(String[] args) {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME); // Esto es para trabajar con OpenCV
-        try {
-            Channel c = ConexionRabbitMQ.getChannel();
-            Gson gson = new Gson();
-            long tsReceptionWorker = System.currentTimeMillis();
+	static int contador = 0;
 
-            try {
-                ConnectionFactory factory = new ConnectionFactory();
-                factory.setUsername(RabbitMQStuff.USER);
-                factory.setPassword(RabbitMQStuff.PASSWD);
-                factory.setVirtualHost(RabbitMQStuff.VIRTUAL_HOST);
-                factory.setHost(RabbitMQStuff.HOST);
-                factory.setPort(RabbitMQStuff.PORT);
-                Connection conn = factory.newConnection();
-                Channel channel = conn.createChannel();
+	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME); // Esto es para trabajar con OpenCV
+		try {
+//            Channel c = ConexionRabbitMQ.getChannel();
+			Gson gson = new Gson();
+			long tsReceptionWorker = System.currentTimeMillis();
 
-                channel.queueDeclare(RabbitMQStuff.COLA_TRABAJOS, true, false, false, null);
-                System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+			ConnectionFactory factory = new ConnectionFactory();
+			factory.setUsername(RabbitMQStuff.USER);
+			factory.setPassword(RabbitMQStuff.PASSWD);
+			factory.setVirtualHost(RabbitMQStuff.VIRTUAL_HOST);
+			factory.setHost(RabbitMQStuff.HOST);
+			factory.setPort(RabbitMQStuff.PORT);
+			Connection conn = factory.newConnection();
+			Channel channel = conn.createChannel();
 
-                Consumer consumer = new DefaultConsumer(channel) {
-                    @Override
-                    public void handleDelivery(String consumerTag, Envelope envelope,
-                            AMQP.BasicProperties properties, byte[] body)
-                            throws IOException {
-                        String message = new String(body, "UTF-8");
-                        System.out.println(" [x] Received '" + message + "'");
-                        ImageJob ij = gson.fromJson(message, ImageJob.class);
+			channel.queueDeclare(RabbitMQStuff.COLA_TRABAJOS, true, false, false, null);
+			System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-                        String[] commands = new String[]{"bash", "-c", "scp twcam@10.50.0.10:" + ij.getPathSrc() + ij.getImageSrc() + " /home/twcam/opencv/images/descargadas/"};
+			Consumer consumer = new DefaultConsumer(channel) {
+				@Override
+				public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+						byte[] body) throws IOException {
+					String message = new String(body, "UTF-8");
+					System.out.println(" [x] Received '" + message + "'");
+					ImageJob ij = gson.fromJson(message, ImageJob.class);
 
-                        ProcessBuilder pb = new ProcessBuilder(commands);
-                        pb.redirectErrorStream(true);
-                        Process process = pb.start();
-                        int exitValue = 5;
-                        try {
-                            exitValue = process.waitFor();
-                            System.out.println(exitValue);
+					// Si la imagen es la 50, se para este worker
+					if (contador == 25) {
+						System.exit(0);
+					}
 
-                            if (exitValue == 0) {
-                                System.out.println(ij.getAction());
+					String[] commands = new String[] { "bash", "-c", "scp twcam@10.50.0.10:" + ij.getPathSrc()
+							+ ij.getImageSrc() + " /home/twcam/opencv/images/descargadas/" };
 
-                                String[] commands2 = new String[]{"bash", "-c", "scp /home/twcam/opencv/images/procesadas/" + ij.getImageDst() + " twcam@10.50.0.10:" + ij.getPathDst()};
-//                                System.out.println(commands);
-                                ProcessBuilder pb2 = new ProcessBuilder(commands2);
-                                pb2.redirectErrorStream(true);
-                                Process process2;
-                                int exitValue2 = 5;
-                                Mat img = OpenCVUtils.readFile("/home/twcam/opencv/images/descargadas/" + ij.getImageSrc());
-                                switch (ij.getAction()) {
-                                    case "blur":
-                                        System.out.println("Voy a hacer un blur");
-                                        Mat blur = OpenCVUtils.blur(img);
-                                        OpenCVUtils.writeImage(blur, "/home/twcam/opencv/images/procesadas/" + ij.getImageDst());
-                                        process2 = pb2.start();
-                                        exitValue2 = process.waitFor();
-                                        break;
-                                    case "gray":
-                                        System.out.println("Voy a hacer un gray");
-                                        Mat gray = OpenCVUtils.gray(img);
-                                        OpenCVUtils.writeImage(gray, "/home/twcam/opencv/images/procesadas/" + ij.getImageDst());
-                                        process2 = pb2.start();
-                                        exitValue2 = process.waitFor();
-                                        break;
-                                    case "edge":
-                                        System.out.println("Voy a hacer un edge");
-                                        Mat edge = OpenCVUtils.canny(img);
-                                        OpenCVUtils.writeImage(edge, "/home/twcam/opencv/images/procesadas/" + ij.getImageDst());
-                                        process2 = pb2.start();
-                                        exitValue2 = process.waitFor();
-                                        break;
-                                    default:
-                                        System.out.println("No estoy haciendo nada");
-                                }
-                                if (exitValue2 == 0) {
-                                    System.out.println("Imagen enviada");
-                                    JobCompletion jc = new JobCompletion("Worker 2", ij.getImageSrc(), ij.getTsCreationMessage(), tsReceptionWorker, System.currentTimeMillis());
-                                    String json = gson.toJson(jc);
+					ProcessBuilder pb = new ProcessBuilder(commands);
+					pb.redirectErrorStream(true);
+					Process process = pb.start();
+					int exitValue = 5;
+					try {
+						exitValue = process.waitFor();
+						System.out.println(exitValue);
 
-                                    // Usamos el canal para definir: el exchange, la cola y la asociación exchange-cola
-                                    c.exchangeDeclare(RabbitMQStuff.EXCHANGE, "direct", true);
-                                    c.queueDeclare(RabbitMQStuff.COLA_TIEMPOS, true, false, false, null);
-                                    c.queueBind(RabbitMQStuff.COLA_TIEMPOS, RabbitMQStuff.EXCHANGE, RabbitMQStuff.RK_TIEMPOS);
+						if (exitValue == 0) {
+							System.out.println(ij.getAction());
 
-                                    // Obtención del cuerpo del mensaje
-                                    byte[] messageBody = json.getBytes();
-                                    // Publicar el mensaje con el trabajo a realizar
-                                    c.basicPublish(RabbitMQStuff.EXCHANGE, RabbitMQStuff.RK_TIEMPOS, null, messageBody);
-                                } else {
-                                    System.err.println("No se ha enviado el archivillo");
-                                }
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        } finally {
+							String[] commands2 = new String[] { "bash", "-c",
+									"scp /home/twcam/opencv/images/procesadas/" + ij.getImageDst()
+											+ " twcam@10.50.0.10:" + ij.getPathDst() };
+							ProcessBuilder pb2 = new ProcessBuilder(commands2);
+							pb2.redirectErrorStream(true);
+							Process process2;
+							int exitValue2 = 5;
+							Mat img = OpenCVUtils.readFile("/home/twcam/opencv/images/descargadas/" + ij.getImageSrc());
+							switch (ij.getAction()) {
+							case "blur":
+								System.out.println("Voy a hacer un blur");
+								Mat blur = OpenCVUtils.blur(img);
+								OpenCVUtils.writeImage(blur,
+										"/home/twcam/opencv/images/procesadas/" + ij.getImageDst());
+								process2 = pb2.start();
+								exitValue2 = process.waitFor();
+								break;
+							case "gray":
+								System.out.println("Voy a hacer un gray");
+								Mat gray = OpenCVUtils.gray(img);
+								OpenCVUtils.writeImage(gray,
+										"/home/twcam/opencv/images/procesadas/" + ij.getImageDst());
+								process2 = pb2.start();
+								exitValue2 = process.waitFor();
+								break;
+							case "edge":
+								System.out.println("Voy a hacer un edge");
+								Mat edge = OpenCVUtils.canny(img);
+								OpenCVUtils.writeImage(edge,
+										"/home/twcam/opencv/images/procesadas/" + ij.getImageDst());
+								process2 = pb2.start();
+								exitValue2 = process.waitFor();
+								break;
+							default:
+								System.out.println("No estoy haciendo nada");
+							}
+							if (exitValue2 == 0) {
+								contador++;
+								System.out.println("Imagen enviada");
+								JobCompletion jc = new JobCompletion("Worker 1", ij.getImageSrc(),
+										ij.getTsCreationMessage(), tsReceptionWorker, System.currentTimeMillis());
+								String json = gson.toJson(jc);
 
-                        }
+								// Usamos el canal para definir: el exchange, la cola y la asociación
+								// exchange-cola
+								channel.exchangeDeclare(RabbitMQStuff.EXCHANGE, "direct", true);
+								channel.queueDeclare(RabbitMQStuff.COLA_TIEMPOS, true, false, false, null);
+								channel.queueBind(RabbitMQStuff.COLA_TIEMPOS, RabbitMQStuff.EXCHANGE,
+										RabbitMQStuff.RK_TIEMPOS);
 
-                    }
-                };
-                channel.basicConsume(RabbitMQStuff.COLA_TRABAJOS, true, consumer);
+								// Obtención del cuerpo del mensaje
+								byte[] messageBody = json.getBytes();
+								// Publicar el mensaje con el trabajo a realizar
+								channel.basicPublish(RabbitMQStuff.EXCHANGE, RabbitMQStuff.RK_TIEMPOS, null,
+										messageBody);
+							} else {
+								System.err.println("No se ha enviado el archivillo");
+							}
+						}
+					} catch (IOException | InterruptedException e) {
+						e.printStackTrace();
+					} finally {
 
-            } catch (IOException ex) {
-                Logger.getLogger(Consumidor.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (TimeoutException ex) {
-                Logger.getLogger(Consumidor.class.getName()).log(Level.SEVERE, null, ex);
-            }
+					}
 
-        } catch (Exception ex) {
-            Logger.getLogger(Consumidor.class.getName()).log(Level.SEVERE, null, ex);
-        }
+				}
+			};
 
-    }
+			channel.basicConsume(RabbitMQStuff.COLA_TRABAJOS, true, consumer);
+
+		} catch (IOException | TimeoutException ex) {
+			Logger.getLogger(Consumidor.class.getName()).log(Level.SEVERE, null, ex);
+		} catch (Exception ex) {
+			Logger.getLogger(Consumidor.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 }
